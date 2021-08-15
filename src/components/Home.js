66c0999109f1a845523f62e56adf7d92af8a1789
koowoy <uk960214@gmail.com>
2021-08-15 13:28:44 +0900

    complete homepage

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,5 +1,19 @@
 import React from "react";
+import image from "../recorder.jpg";
 
 export default function Home() {
-  return <h1>Home Page</h1>;
+  return (
+    <main>
+      <img
+        src={image}
+        alt="recorder"
+        className="absolute object-cover w-full h-full"
+      />
+      <section className="relative flex justify-center min-h-screen pt-12 lg: pt-64 px-8">
+        <h1 className="text-6xl text-green-100 font-bold cursive leading-none lg:leading-snug home-name">
+          Welcome to my world!
+        </h1>
+      </section>
+    </main>
+  );
 }
